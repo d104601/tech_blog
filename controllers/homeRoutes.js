17390308c641f2b3d826246ec6b8e6b7041e7f23
Taeyong Lee <d104601@hotmail.com>
2022-06-23 03:48:36 -0400

    views

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,20 +1,42 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Post } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get("/", async (req,res) => {});
+router.get("/", withAuth, async (req,res) => {
+  try {
+    const postData = await Post.findAll({
+      attributes: ['id', 'time', 'title', 'content'],
+      include: [
+        {
+          model: Comment,
+          attributes: ["id", "time", "content", "postId", "userId"],
+          include: {
+            model: User,
+            attributes: ["username"]
+          }
+        }
+      ]
+    });
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  
+});
 
 router.get("/dashboard", async (req, res) => {});
 
 router.get('/login', async (req, res) => {
-    if (req.session.logged_in) {
+  if (req.session.logged_in) {
       res.redirect('/');
       return;
-    }
+  }
 
-    res.render('login');
-  });
+  res.render('login');
+});
 
 router.get("/logout", async (req, res) => {
     
-})
+});
+
+module.exports = router;
